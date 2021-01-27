a1c97a2bc42f077e3d3708734866875e7567a0d8
Yehiel Dahan <dc02dc03@gmail.com>
2021-01-27 15:30:49 +0200

    targil

---
+++ targil1.py
@@ -0,0 +1,4 @@
+name="yehiel dahan"
+age=28
+city="megdal haemek"
+print("my name is " + name + "\nHow old are you: " + str(age) + "\nResidence: " + city)
---
+++ targil2.py
@@ -0,0 +1,2 @@
+name=input("my name is: ")
+print=(name)
