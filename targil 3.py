d3958ab1c469fd2350b12aeb1f748c75c102a757
Yehiel Dahan <dc02dc03@gmail.com>
2021-01-28 12:20:46 +0200

    targil4

--- targil 3.py
+++ targil 3.py
@@ -1,5 +1,6 @@
 num=4567
-print("Alafim=" + str (num//1000))
-print("Meot=" + str((num%1000)//100))
-print("Asarot=" + str((num%100)//10))
-print("Ahadot=" + str(num%10))
+# print("Alafim=" + str (num//1000))
+# print("Meot=" + str((num%1000)//100))
+# print("Asarot=" + str((num%100)//10))
+# print("Ahadot=" + str(num%10))
+print("Alafim=" + str (num //1000) + "\nMeot=" + str((num%1000)//100) + "\nAsarot=" + str((num%100)//10) + "\nAhadot=" + str(num%10))
